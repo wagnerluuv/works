2ea00e73e7b34464d343ab3d7b03e6b2a6bde32b
Felix Wagner <felixwagner23@outlook.com>
2020-03-07 01:14:44 +0100

    adobe cs6 crack helper project initialization

---
+++ AdobeCrackHelper/AdobeCrackHelper/CrackHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdobeCrackHelper
+{
+    public static class CrackHelper
+    {
+        public static void GetFilePath(this TextBox textbox)
+        {
+            using(OpenFileDialog dialog = new OpenFileDialog { Filter = "library files (*.dll)|*.dll" })
+            {
+                if(dialog.ShowDialog() == DialogResult.OK)
+                {
+                    textbox.Text = dialog.FileName;
+                }
+            }
+        }
+
+        internal static void Replace(string file32, string file64)
+        {
+            replaceAmtlib(Environment.ExpandEnvironmentVariables("%ProgramFiles(x86)%"), file32);
+            replaceAmtlib(Environment.ExpandEnvironmentVariables("%ProgramW6432%"), file64);
+        }
+
+        private static void replaceAmtlib(string folder, string file)
+        {
+            string[] files = getFiles(folder);
+
+            foreach(string amtlib in files)
+            {
+                if(MessageBox.Show($"replace {amtlib} ?", "confirm please", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    File.Delete(amtlib);
+                    File.Copy(file, amtlib);
+                }
+            }
+        }
+
+        private static string[] getFiles(string folder)
+        {
+            List<string> files = new List<string>();
+            try
+            {
+                files.AddRange(Directory.GetFiles(folder, "*amtlib.dll", SearchOption.TopDirectoryOnly)
+                .Where(amtlib => amtlib.Contains("adobe") || amtlib.Contains("Adobe")));
+
+                files.AddRange(Directory.GetDirectories(folder).SelectMany(subFolder => getFiles(subFolder)));
+            }
+            catch
+            {
+
+            }
+            return files.ToArray();
+        }
+    }
+}
---
+++ AdobeCrackHelper/AdobeCrackHelper/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace AdobeCrackHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFile64 = new System.Windows.Forms.TextBox();
+            this.buttonBrowse64 = new System.Windows.Forms.Button();
+            this.buttonBrowse32 = new System.Windows.Forms.Button();
+            this.textBoxFile32 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonReplace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "cracked amtlib.dll 64bit";
+            // 
+            // textBoxFile64
+            // 
+            this.textBoxFile64.Location = new System.Drawing.Point(15, 25);
+            this.textBoxFile64.Name = "textBoxFile64";
+            this.textBoxFile64.Size = new System.Drawing.Size(318, 22);
+            this.textBoxFile64.TabIndex = 1;
+            // 
+            // buttonBrowse64
+            // 
+            this.buttonBrowse64.Location = new System.Drawing.Point(339, 25);
+            this.buttonBrowse64.Name = "buttonBrowse64";
+            this.buttonBrowse64.Size = new System.Drawing.Size(76, 22);
+            this.buttonBrowse64.TabIndex = 2;
+            this.buttonBrowse64.Text = "browse";
+            this.buttonBrowse64.UseVisualStyleBackColor = true;
+            this.buttonBrowse64.Click += new System.EventHandler(this.buttonBrowse64_Click);
+            // 
+            // buttonBrowse32
+            // 
+            this.buttonBrowse32.Location = new System.Drawing.Point(339, 89);
+            this.buttonBrowse32.Name = "buttonBrowse32";
+            this.buttonBrowse32.Size = new System.Drawing.Size(76, 22);
+            this.buttonBrowse32.TabIndex = 5;
+            this.buttonBrowse32.Text = "browse";
+            this.buttonBrowse32.UseVisualStyleBackColor = true;
+            this.buttonBrowse32.Click += new System.EventHandler(this.buttonBrowse32_Click);
+            // 
+            // textBoxFile32
+            // 
+            this.textBoxFile32.Location = new System.Drawing.Point(15, 89);
+            this.textBoxFile32.Name = "textBoxFile32";
+            this.textBoxFile32.Size = new System.Drawing.Size(318, 22);
+            this.textBoxFile32.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "cracked amtlib.dll 32bit";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(339, 152);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 22);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "browse";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 152);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(318, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(339, 217);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 22);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "browse";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 217);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(318, 22);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // buttonReplace
+            // 
+            this.buttonReplace.Location = new System.Drawing.Point(140, 283);
+            this.buttonReplace.Name = "buttonReplace";
+            this.buttonReplace.Size = new System.Drawing.Size(76, 22);
+            this.buttonReplace.TabIndex = 12;
+            this.buttonReplace.Text = "replace";
+            this.buttonReplace.UseVisualStyleBackColor = true;
+            this.buttonReplace.Click += new System.EventHandler(this.buttonReplace_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 521);
+            this.Controls.Add(this.buttonReplace);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonBrowse32);
+            this.Controls.Add(this.textBoxFile32);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonBrowse64);
+            this.Controls.Add(this.textBoxFile64);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "AdobeCrackHelper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFile64;
+        private System.Windows.Forms.Button buttonBrowse64;
+        private System.Windows.Forms.Button buttonBrowse32;
+        private System.Windows.Forms.TextBox textBoxFile32;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonReplace;
+    }
+}
+
