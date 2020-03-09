00d375a5f704e9a0121a40267e141f014328a4cb
Felix Wagner <felixwagner23@outlook.com>
2020-03-09 21:13:33 +0100

    deleted some obsolete comments

--- AdobeCrackHelper/AdobeCrackHelper/Program.cs
+++ AdobeCrackHelper/AdobeCrackHelper/Program.cs
@@ -8,9 +8,6 @@ namespace AdobeCrackHelper
 {
     static class Program
     {
-        /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
-        /// </summary>
         [STAThread]
         static void Main()
         {
